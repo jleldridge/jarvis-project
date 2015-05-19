c9d3320c5fd0bfc6c0387c030a61e4c5a6a88c61
jleldridge <jleldridge27@gmail.com>
2015-05-18 22:37:30 -0400

    Got simple 3d text wrapping working on 2 layers.

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Hello world");
+var IntrospectionEngine = require("IntrospectionEngine");
+var fs = require('fs');
+
+var express = require('express');
+var app = express();
+
+var self = this;
+
+app.use(express.static('js'));
+
+app.get('/', function (req, res){
+    res.setHeader('Content-Type', 'text/html');
+    res.send(fs.readFileSync('./html/index.html'));
+});
+
+var server = app.listen(8080, function (){
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Express server started at http://%s:%s', host, port);
+});
